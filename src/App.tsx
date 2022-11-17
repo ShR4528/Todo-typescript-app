986124d64989f27508b18a8c9a8ae34366ff1161
Shr4528 <Shr4528@gmail.com>
2022-11-17 13:03:28 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TodoForm } from './Components/TodoForm';
 import './App.css';
+import {TodoList } from './Components/TodoList';
+ 
 
 function App() {
+  const [todos, setTodos ] = useState<Array<Todo>>([]);
+
+  const addTodo: AddTodo = newTodo => {
+    if(newTodo !== "") {
+      setTodos([...todos, {text: newTodo, complete: false}]);
+    }
+  };
+
+  const toggleComplete: ToggleComplete = selectedTodo => {
+    const updatedTodos = todos.map(todo => {
+      if(todo === selectedTodo) {
+        return {...todo, complete: !todo.complete};
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <TodoForm  addTodo={addTodo}/>
+      <TodoList  todos={todos} toggleComplete={toggleComplete} />
     </div>
   );
 }
